7d62a7a29d1a53eab1ced1823a03143653adb78f
Owen Shepherd <owen.shepherd@e43.eu>
2010-07-06 02:28:23 +0100

    Prepare repo interface for the new browser updates. TODO: HG support

--- eforge/browser/interface.py
+++ eforge/browser/interface.py
@@ -1,78 +1,80 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+
 class IRepository(object):
-    """ 
+    """
         EForge source browser repository access interface.
-        
+
         This is the interface by which EForge will access (to inspect) the
         repository.
     """
-    
+
     def __init__(self, path):
-        """ 
-            Constructs a new Repository object. path should be the path to the 
+        """
+            Constructs a new Repository object. path should be the path to the
             repository being opened.
         """
-        
-    
+
+
     @property
     def branches(self):
         """
             Returns a dictionary containing a key for every branch in the
             repository with a corresponding value for that branch's head
             revision
-               
+
             The head-revision should be returned as an IRevision subclass.
-            
+
             If branches are not supported, should return an empty list.
         """
         pass
-    
+
     @property
     def tags(self):
-        """ 
+        """
             Returns a dictionary containing a key for every tag in the
             repository with a corresponding value for that tag's revision
-                
+
             The revision should be returned as an IRevision subclass.
-            
+
             If tags are not supported, should return an empty list.
         """
         pass
-    
+
     def revision(self, id):
         """
-            Given the specified id, returns a revision (IRevision) object 
+            Given the specified id, returns a revision (IRevision) object
             corresponding to it.
         """
         pass
-    
+
     @property
     def head(self):
         """
             Returns the head revision of the repository. This is commonly the
-            most recent revision on the "head" or "trunk" branch. 
-            
-            For example, for Git this would be the revision pointed to by the 
+            most recent revision on the "head" or "trunk" branch.
+
+            For example, for Git this would be the revision pointed to by the
             "HEAD" ref; on Subversion this would be the last commit to "trunk".
         """
         pass
-    
+
     @property
     def revisions(self):
         """
             Returns a list of all revisions in the repository. The order is
             unspecified.
-            
+
             Because revision lists can grow quite large, it is recommended that
             implementations use a custom iterator to avoid loading the whole
             repository into memory.
         """
-        
+
 class IRevision(object):
     """
         Repository changeset/revision.
     """
-    
+
     @property
     def id(self):
         """
@@ -81,53 +83,76 @@ class IRevision(object):
             to get an object corresponding to the same revision.
         """
         pass
-        
+
     @property
     def short_id(self):
         """
-            The short ID of this revision. This should be a shortened (but 
+            The short ID of this revision. This should be a shortened (but
             nontheless unambiguous) ID for a revision in the repository, which
             it should be possible to use in the same way the value of id would
             be.
-            
+
             Some systems (Such as Subversion) have concise monotonic IDs and
-            should return the same value for short_id and id. Others, 
+            should return the same value for short_id and id. Others,
             particularly distributed systems (Such as hg and git) use 40
             character SHA-1 hashes as identifiers; in these cases it is valuable
             to return a truncated version for brevity.
-            
+
             The default implementation returns id.
         """
         return self.id
-        
+
     @property
     def author_email(self):
         """
             Returns the E-Mail address of the author (if known)
         """
         pass
-        
+
     @property
     def author_name(self):
         """
             Returns the name of the author (if known)
         """
         return self.author_email
-        
+
+    @property
+    def author_user(self):
+        """
+            Returns the user of the author (if known)
+        """
+        try:
+            return User.objects.get(email=self.author_email)
+        except User.NotFoundException:
+            return None
+
+    @property
+    def message(self):
+        """ Commit message """
+        pass
+
+    @property
+    def short_message(self):
+        """ Short message """
+        try:
+            return self.message.split('.', 1)[0].strip()
+        except:
+            return self.message
+
     @property
     def parents(self):
         """
             Returns a list of the revisions that this revision is descended from.
         """
         pass
-        
+
     @property
     def children(self):
         """
             Returns a list of the revisions that are descended from this revision.
         """
         pass
-    
+
     @property
     def root(self):
         """
@@ -140,12 +165,12 @@ class IGenericFile(object):
         A generic file object. This is the base class of IDirectory and
         IFile. It provides some trivial methods.
     """
-    
+
     def __init__(self, parent, name):
         """
             Constructs the generic file. parent should be the directory which
-            is parent of this one (if not the root, in which case pass None). 
-            
+            is parent of this one (if not the root, in which case pass None).
+
             The name parameter should be the name of the file relative to the
             parent directory. For the root, the name is ignored
         """
@@ -157,14 +182,14 @@ class IGenericFile(object):
             self.path   = ''
             self.name   = None
             self.pretty = 'Root'
-       
+
     @property
     def is_file(self):
-        """ 
-            Is this a file? 
+        """
+            Is this a file?
         """
         return not self.is_directory
-        
+
     @property
     def is_directory(self):
         """
@@ -178,40 +203,40 @@ class IDirectory(IGenericFile):
     """
     def __init__(self, *args, **kwargs):
         IGenericFile.__init__(self, *args, **kwargs)
-    
+
     @property
     def is_directory(self):
         return True
-        
+
 
     @property
     def children(self):
         """ A list of the immediate children of this directory """
         pass
-        
+
     def child(self, name):
         """ Get the child with the given name"""
         pass
-        
+
 class IFile(IGenericFile):
     """
         A file within the repository.
-        
+
         In addition to the methods documented here, this should be implemented
         as a file-like object.
-        
+
         Finally, the "data" property should be a string of the contents of the
         file
     """
-    
+
     def __init__(self, *args, **kwargs):
         IGenericFile.__init__(self, *args, **kwargs)
         self.__analysed = False
-    
+
     @property
     def is_file(self):
         return True
-        
+
     @property
     def size(self):
         """ The size, in bytes, of the file """
@@ -221,59 +246,59 @@ class IFile(IGenericFile):
     def is_binary(self):
         """
             Is this a binary file?
-            
+
             For systems which do not track this, a herustic is provided.
         """
         if not self.__analysed:
             self.__file_type()
         return self.__is_binary
-        
+
     @property
     def is_text(self):
         """
             Is this a text file?
-            
+
             For systems which do not track this, a herustic is provided.
         """
         return not self.is_binary
-        
+
     @property
     def text_encoding(self):
         if not self.__analysed:
             self.__file_type()
         return self.__text_encoding
-        
+
 
     @property
     def _default_encoding(self):
         return 'UTF8'
-    
+
     def __file_type(self):
         # Guess the type of a file
-        
+
         pos = self.tell()
         self.seek(0)
         dat = self.read(1024)
-        
+
         if dat.startswith('\xFE\xFF'):
             self.__is_binary     = False
             self.__text_encoding = 'UTF16-BE'
-            
+
         elif dat.startswith('\xFF\xFE'):
             self.__is_binary     = False
             self.__text_encoding = 'UTF16-LE'
-            
+
         elif dat.startswith('\xEF\xBB\xBF'):
             self.__is_binary     = False
             self.__text_encoding = 'UTF8'
-            
+
         elif '\x00' in dat:
             self.__is_binary     = True
-            
+
         else:
             # Text of some unknown form
             self.__is_binary     = False
             self.__text_encoding = self._default_encoding
-            
+
         self.seek(pos)
         self.__analysed = True
--- eforge/browser/vcs/git/repo.py
+++ eforge/browser/vcs/git/repo.py
@@ -3,7 +3,7 @@ import dulwich
 import StringIO
 from eforge.browser.interface import *
 
-class GitRepository(IRepository):   
+class GitRepository(IRepository):
     def __init__(self, path):
         self.repo = dulwich.repo.Repo(path)
 
@@ -13,75 +13,89 @@ class GitRepository(IRepository):
         for ref in grefs.keys():
             if ref.startswith(prefix):
                 refs[ref[len(prefix):]] = GitRevision(self, self.repo.refs.follow(ref))
-                
+
         return refs
-    
+
     @property
     def branches(self):
         return self.__findrefs('refs/heads/')
-    
+
     @property
     def tags(self):
         return self.__findrefs('refs/tags/')
-    
+
     def revision(self, id):
         return GitRevision(self, id)
-    
+
     @property
     def head(self):
         return GitRevision(self, self.repo.head())
-    
+
     @property
     def revisions(self):
-        return [GitRevision(self, x) for x in self.repo.revision_history()]
-        
+        revs = []
+
+        walk = self.repo.get_graph_walker()
+        ob = walk.next()
+        while ob:
+            revs.append(GitRevision(self, ob))
+            ob = walk.next()
+
+        return revs
+
 class GitRevision(IRevision):
     def __init__(self, repo, rev):
         self.repo = repo
-        self.rev  = repo.repo[rev]
-    
+        if not isinstance(rev, dulwich.objects.Commit):
+            self.rev  = repo.repo[rev]
+        else:
+            self.rev = rev
+
     @property
     def id(self):
         return self.rev.id
-        
-        
+
+
     @property
     def short_id(self):
         return self.id[0:8]
-        
+
     @property
     def author_email(self):
         try:
-            return self.rev.author.split('<')[1][:1]
+            return self.rev.author.split('<')[1][:-1]
         except:
             return None
-            
-        
+
+    @property
+    def message(self):
+        return self.rev.message
+
     @property
     def author_name(self):
         try:
-            return self.rev.author.split('<').trim()
+            return self.rev.author.split('<')[0].strip()
         except:
-            return self.author_email
-        
+            return self.rev.author
+
     @property
     def parents(self):
         return [GitRevision(self.repo, x) for x in self.rev.parents]
-        
+
     @property
     def children(self):
         return []
-    
+
     @property
     def root(self):
         return GitDirectory(self.repo, self.repo.repo[self.rev.tree], None, None)
-        
+
 class GitDirectory(IDirectory):
     def __init__(self, repo, tree, parent, name):
         self.repo = repo
         self.tree = tree
         IDirectory.__init__(self, parent, name)
-    
+
     @property
     def children(self):
         children = []
@@ -99,28 +113,28 @@ class GitDirectory(IDirectory):
                     children.append(GitFile(self.repo, obj, self, name))
             except Exception, e:
                 print "Exception: %s" % e
-                
+
         return children
-        
+
     def child(self, name):
         obj = self.repo.repo[self.tree[name][1]]
         if isinstance(obj, dulwich.objects.Tree):
             return GitDirectory(self.repo, obj, self, name)
         else:
             return GitFile(self.repo, obj, self, name)
-            
+
 class GitFile(IFile, StringIO.StringIO):
     def __init__(self, repo, file, parent, name):
         self.repo = repo
         self.file = file
-        
+
         self.data = file.data
-        
+
         IFile.__init__(self, parent, name)
         StringIO.StringIO.__init__(self, self.data)
         self.__size = len(self.data)
-        
+
     @property
     def size(self):
         return self.__size
-    
+
